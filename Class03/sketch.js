2f036dcfb6990a5081a8385a83875f523d31623f
gamingar-tony <araems1@lsu.edu>
2024-01-30 14:07:42 -0600

    Started Drawing

--- Class03/sketch.js
+++ Class03/sketch.js
@@ -1,9 +1,39 @@
+let purple;
+let gold;
+let x;
+let y;
+let size;
+
 function setup()
 {
   createCanvas(400, 400);
+  purple = color('#461D7C');
+  gold = color('#FDD023');
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw()
 {
   background(220);
+
+  if (mouseIsPressed)
+  {
+    if (mouseX >= x)
+    {
+      fill(purple);
+      stroke(gold);
+
+      x += mouseX - pmouseX;
+      y += mouseY - pmouseY;
+    }
+  }
+
+  else
+  {
+    fill(gold);
+    stroke(0);
+  }
+
+  square(x, y, 100);
 }
