8501da3e6b76a1d5607f192f908832e7698acc6c
gamingar-tony <araems1@lsu.edu>
2024-01-31 08:27:51 -0600

    Added colors needed for the assignment

--- Assignment1.2/sketch.js
+++ Assignment1.2/sketch.js
@@ -1,7 +1,40 @@
-function setup() {
-  createCanvas(400, 400);
+function setup()
+{
+  createCanvas(1000, 1000);
 }
 
-function draw() {
-  background(220);
+function draw()
+{
+  background(255);
+  noStroke();
+  
+  fill('red');
+  square(0, 0, 50);
+
+  fill('orange');
+  square(0, 50, 50);
+
+  fill('yellow');
+  square(0, 100, 50);
+
+  fill('green');
+  square(0, 150, 50);
+
+  fill('cyan');
+  square(0, 200, 50);
+
+  fill('blue');
+  square(0, 250, 50);
+
+  fill('magenta');
+  square(0, 300, 50);
+
+  fill('brown');
+  square(0, 350, 50);
+
+  fill('white');
+  square(0, 400, 50);
+
+  fill('black');
+  square(0, 450, 50);
 }
