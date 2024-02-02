add7f98ce7232e6b08c25e2d33a87deace3b8ea2
gamingar-tony <araems1@lsu.edu>
2024-02-02 08:13:15 -0600

    Updated Class03 file

--- Class03/sketch.js
+++ Class03/sketch.js
@@ -2,38 +2,64 @@ let purple;
 let gold;
 let x;
 let y;
-let size;
+const size = 100;
+let dragging = false;
 
 function setup()
 {
   createCanvas(400, 400);
+
   purple = color('#461D7C');
   gold = color('#FDD023');
   x = width / 2;
   y = height / 2;
+
+  // rectMode(CENTER);
 }
 
 function draw()
 {
   background(220);
 
-  if (mouseIsPressed)
+  // if (mouseIsPressed)
+  // {
+  //   if (mouseX >= x && mouseX <= x + size && mouseY >= y && mouseY <= y + size)
+  //   {
+  //     fill(purple);
+  //     stroke(gold);
+
+  //     x += mouseX - pmouseX;
+  //     y += mouseY - pmouseY;
+  //   }
+  // }
+
+  // else
+  // {
+  //   fill(gold);
+  //   stroke(0);
+  // }
+
+  square(x, y, size);
+}
+
+function mousePressed()
+{
+  if (mouseX >= x && mouseX <= x + size && mouseY >= y && mouseY <= y + size)
   {
-    if (mouseX >= x)
-    {
-      fill(purple);
-      stroke(gold);
-
-      x += mouseX - pmouseX;
-      y += mouseY - pmouseY;
-    }
+    dragging = true;
   }
+}
 
-  else
+function mouseReleased()
+{
+  dragging = false;
+}
+
+function mouseDragged()
+{
+  if (dragging)
   {
-    fill(gold);
-    stroke(0);
+    x += mouseX - pmouseX;
+    y += mouseY - pmouseY;
   }
-
-  square(x, y, 100);
 }
