0309feed78a508ccd20ab8cb979b2c58c8901b7a
gamingar-tony <araems1@lsu.edu>
2024-02-05 14:49:07 -0600

    Edited everything up until drawing on the canvas

--- Assignment1.2/sketch.js
+++ Assignment1.2/sketch.js
@@ -1,34 +1,81 @@
 let selectedColor;
 let colorPallete;
+let x;
+let y;
+let insideX;
+let insideY;
+let isInColor;
+let i;
 
 function setup()
 {
-    createCanvas(400, 400);
+    createCanvas(725, 750);
 
     colorPallete =
     [
-        new Color(color('red')),
-        new Color(color('orange')),
-        new Color(color('yellow')),
-        new Color(color('green')),
-        new Color(color('cyan')),
-        new Color(color('blue')),
-        new Color(color('magenta')),
-        new Color(color('brown')),
-        new Color(color('white')),
-        new Color(color('black'))
+        new Color(0, 0, 50, color('red')),
+        new Color(0, 50, 50, color('orange')),
+        new Color(0, 100, 50, color('yellow')),
+        new Color(0, 150, 50, color('green')),
+        new Color(0, 200, 50, color('cyan')),
+        new Color(0, 250, 50, color('blue')),
+        new Color(0, 300, 50, color('magenta')),
+        new Color(0, 350, 50, color('brown')),
+        new Color(0, 400, 50, color('white')),
+        new Color(0, 450, 50, color('black'))
     ];
+
+    selectedColor = color('white');
+}
+
+function draw()
+{
+    background(255);
+
+    for (i = 0; i < colorPallete.length; i++)
+    {
+        colorPallete[i].draw();
+    }
+
+    fill(selectedColor);
+    circle(width - 30, 30, 20);
+}
+
+function mousePressed()
+{
+    isInColor = false;
+
+    for (i = 0; i < colorPallete.length; i++)
+    {
+        if (colorPallete[i].contains(mouseX, mouseY))
+        {
+            selectedColor = colorPallete[i].fill;
+            isInColor = true;
+        }
+    }
 }
 
 class Color
 {
-    constructor (fill)
+    constructor (x, y, diameter, fill)
     {
         this.fill = fill;
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
     }
 
     draw()
     {
         fill(this.fill);
+        square(this.x, this.y, this.diameter);
+    }
+
+    contains(x, y)
+    {
+        insideX = x >= this.x && x <= this.x + 50;
+        insideY = y >= this.y && y <= this.y + 50;
+
+        return insideX && insideY;
     }
 }
