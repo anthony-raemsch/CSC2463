30fdb04767743cf11b31f8d36a327395a05022d2
gamingar-tony <araems1@lsu.edu>
2024-02-15 15:58:27 -0600

    Set collider for sprites to 'none'

--- Class07/sketch.js
+++ Class07/sketch.js
@@ -1,17 +1,12 @@
-let animations =
-{
-  stand: {row: 0, frames: 1},
-  walkRight: {row: 0, col: 1, frames: 8},
-  walkUp: {row: 5, frames: 6},
-  walkDown: {row: 5, col: 6, frames: 6}
-};
+// let animal;
 
 class Character
 {
-  constructor (x, y, width, height, spriteSheet, animations)
+  constructor(x, y, width, height, spriteSheet, animations)
   {
     this.sprite = new Sprite(x, y, width, height);
     this.sprite.spriteSheet = spriteSheet;
+    this.sprite.collider = 'none';
     this.sprite.anis.frameDelay = 8;
     this.sprite.addAnis(animations);
     this.sprite.changeAni('stand');
@@ -42,7 +37,7 @@ class Character
   walkRight()
   {
     this.sprite.changeAni('walkRight');
-    this.sprite.vel.x = 1;  
+    this.sprite.vel.x = 1;
     this.sprite.scale.x = 1;
     this.sprite.vel.y = 0;
   }
@@ -61,76 +56,67 @@ function draw()
 {
   background(0);
 
+  // text(animal, 50, 50);
+
   characters.forEach((character) =>
-    {
-      if (kb.pressing('d'))
-      {
-        character.walkRight();
-      }
-
-      else if (kb.pressing('a'))
-      {
-        character.walkLeft();
-      }
-
-      else if (kb.pressing('w'))
-      {
-        character.walkUp();
-      }
-
-      else if (kb.pressing('s'))
-      {
-        character.walkDown();
-      }
-
-      else
-      {
-        character.stop();
-      }
-
-      if (character.sprite.x + character.sprite.width / 4 > width)
-      {
-        character.walkLeft();
-      }
-
-      else if (character.sprite.x - character.sprite.width / 4 < 0)
-      {
-        character.walkRight();
-      }
-    })
-}
-
-function keyTypedOld()
-{
-  switch(key)
   {
-    case 'd':
-      walkRight();
-      break;
+    if (kb.pressing('d'))
+    {
+      character.walkRight();
+    }
 
-    case 'a':
-      walkLeft();
-      break;
+    else if (kb.pressing('a'))
+    {
+      character.walkLeft();
+    }
 
-    case 'w':
-      walkUp();
-      break;
+    else if (kb.pressing('w'))
+    {
+      character.walkUp();
+    }
 
-    case 's':
-      walkDown();
-      break;
-  }
+    else if (kb.pressing('s'))
+    {
+      character.walkDown();
+    }
+
+    else
+    {
+      character.stop();
+    }
+
+    if (character.sprite.x + character.sprite.width / 4 > width)
+    {
+      character.walkLeft();
+    }
+
+    else if (character.sprite.x - character.sprite.width / 4 < 0)
+    {
+      character.walkRight();
+    }
+  })
 }
 
 function preload()
 {
+  let animations =
+  {
+    stand: {row: 0, frames: 1},
+    walkRight: {row: 0, col: 1, frames: 8},
+    walkUp: {row: 5, frames: 6},
+    walkDown: {row: 5, col: 6, frames: 6}
+  };
+
   characters.push(new Character(100, 100, 80, 80, 'assets/cyclops.png', animations));
   characters.push(new Character(200, 200, 80, 80, 'assets/cyclops.png', animations));
 }
 
 function setup()
 {
-  createCanvas(2519, 1339);
+  createCanvas(2519, 1338);
+
+  // let animals = ['🦁', '🐯', '🐻'];
+  // animal = random(animals);
 }
 
 let sprite;
