7037cf5a8c0abb3dd2418010555595fb77a06668
gamingar-tony <araems1@lsu.edu>
2024-02-15 16:20:04 -0600

    Added Character Class

--- Assignment1.3/sketch.js
+++ Assignment1.3/sketch.js
@@ -1,93 +1,110 @@
-let animations =
+class Character
 {
-  stand: {row: 0, frames: 1},
-  walkRight: {row: 0, col: 1, frames: 8},
-  walkUp: {row: 5, frames: 6},
-  walkDown: {row: 5, col: 6, frames: 6}
+  constructor (x, y, width, height, spriteSheet, animations)
+  {
+    this.spelunkyCharacter = new Sprite(x, y, width, height);
+    this.spelunkyCharacter.spriteSheet = spriteSheet;
+    this.spelunkyCharacter.anis.frameDelay = 8;
+    this.spelunkyCharacter.addAnis(animations);
+    this.spelunkyCharacter.changeAni('stand');
+  }
+
+  stop()
+  {
+    this.spelunkyCharacter.changeAni('stand');
+    this.spelunkyCharacter.vel.x = 0;
+    this.spelunkyCharacter.vel.y = 0;
+  }
+
+  walkDown()
+  {
+    this.spelunkyCharacter.changeAni('walkDown');
+    this.spelunkyCharacter.vel.y = 1;
+    this.spelunkyCharacter.vel.x = 0;
+  }
+
+  walkLeft()
+  {
+    this.spelunkyCharacter.changeAni('walkRight');
+    this.spelunkyCharacter.vel.x = -1;
+    this.spelunkyCharacter.vel.y = 0;
+    this.spelunkyCharacter.scale.x = -1;
+  }
+
+  walkRight()
+  {
+    this.spelunkyCharacter.changeAni('walkRight');
+    this.spelunkyCharacter.vel.x = 1;
+    this.spelunkyCharacter.vel.y = 0;
+    this.spelunkyCharacter.scale.x = 1;
+  }
+
+  walkUp()
+  {
+    this.spelunkyCharacter.changeAni('walkUp');
+    this.spelunkyCharacter.vel.x = 0;
+    this.spelunkyCharacter.vel.y = -1;
+  }
 }
+
+let characters = [];
 
 function draw()
 {
   background(255);
 
-  if (kb.pressing('d'))
+  characters.forEach((character) =>
   {
-    walkRight();
-  }
+    if (kb.pressing('d'))
+    {
+      character.walkRight();
+    }
 
-  else if (kb.pressing('a'))
-  {
-    walkLeft();
-  }
+    else if (kb.pressing('a'))
+    {
+      character.walkLeft();
+    }
 
-  else if (kb.pressing('w'))
-  {
-    walkUp();
-  }
+    else if (kb.pressing('w'))
+    {
+      character.walkUp();
+    }
 
-  else if (kb.pressing('s'))
-  {
-    walkDown();
-  }
+    else if (kb.pressing('s'))
+    {
+      character.walkDown();
+    }
 
-  else
-  {
-    stop();
-  }
+    else
+    {
+      character.stop();
+    }
 
-  if (green.x + green.width / 4 > width)
-  {
-    walkLeft();
-  }
+    if (character.spelunkyCharacter.x + character.spelunkyCharacter.width / 4 > width)
+    {
+      character.walkLeft();
+    }
 
-  else if (green.x - green.width / 4 < 0)
-  {
-    walkRight();
-  }
-
-  if (ninja.x + ninja.width / 4 > width)
-  {
-    walkLeft();
-  }
-
-  else if (ninja.x - ninja.width / 4 < 0)
-  {
-    walkRight();
-  }
-
-  if (spelunky.x + spelunky.width / 4 > width)
-  {
-    walkLeft();
-  }
-
-  else if (spelunky.x - spelunky.width / 4 < 0)
-  {
-    walkRight();
-  }
+    else if (character.spelunkyCharacter.x - character.spelunkyCharacter.width / 4 < 0)
+    {
+      character.walkRight();
+    }
+  })
 }
-
-let green;
-let ninja;
 
 function preload()
 {
-  green = new Sprite(200, 200, 80, 80);
-  green.spriteSheet = 'assets/green.png';
-  green.anis.frameDelay = 8;
-  green.addAnis(animations);
-  green.changeAni('stand');
+  let animations =
+  {
+    stand: {row: 0, frames: 1},
+    walkRight: {row: 0, col: 1, frames: 8},
+    walkUp: {row: 5, frames: 6},
+    walkDown: {row: 5, col: 6, frames: 6}
+  }
 
-  ninja = new Sprite(200, 200, 80, 80);
-  ninja.spriteSheet = 'assets/ninja.png';
-  ninja.anis.frameDelay = 8;
-  ninja.addAnis(animations);
-  ninja.changeAni('stand');
-
-  spelunky = new Sprite(200, 200, 80, 80);
-  spelunky.spriteSheet = 'assets/spelunky.png';
-  spelunky.anis.frameDelay = 8;
-  spelunky.addAnis(animations);
-  spelunky.changeAni('stand');
+  characters.push(new Character(100, 100, 80, 80, 'assets/green.png', animations));
+  characters.push(new Character(200, 200, 80, 80, 'assets/ninja.png', animations));
+  characters.push(new Character(300, 300, 80, 80, 'assets/spelunky.png', animations));
 }
 
 function setup()
@@ -95,85 +112,4 @@ function setup()
   createCanvas(2519, 1338);
 }
 
-let spelunky;
-
-function stop()
-{
-  green.changeAni('stand');
-  green.vel.x = 0;
-  green.vel.y = 0;
-
-  ninja.changeAni('stand');
-  ninja.vel.x = 0;
-  ninja.vel.y = 0;
-
-  spelunky.changeAni('stand');
-  spelunky.vel.x = 0;
-  spelunky.vel.y = 0;
-}
-
-function walkDown()
-{
-  green.changeAni('walkDown');
-  green.vel.y = 1;
-  green.vel.x = 0;
-
-  ninja.changeAni('walkDown');
-  ninja.vel.y = 1;
-  ninja.vel.x = 0;
-
-  spelunky.changeAni('walkDown');
-  spelunky.vel.y = 1;
-  spelunky.vel.x = 0;
-}
-
-function walkLeft()
-{
-  green.changeAni('walkRight');
-  green.vel.x = -1;
-  green.scale.x = -1;
-  green.vel.y = 0;
-
-  ninja.changeAni('walkRight');
-  ninja.vel.x = -1;
-  ninja.scale.x = -1;
-  ninja.vel.y = 0;
-
-  spelunky.changeAni('walkRight');
-  spelunky.vel.x = -1;
-  spelunky.scale.x = -1;
-  spelunky.vel.y = 0;
-}
-
-function walkRight()
-{
-  green.changeAni('walkRight');
-  green.vel.x = 1;  
-  green.scale.x = 1;
-  green.vel.y = 0;
-
-  ninja.changeAni('walkRight');
-  ninja.vel.x = 1;  
-  ninja.scale.x = 1;
-  ninja.vel.y = 0;
-
-  spelunky.changeAni('walkRight');
-  spelunky.vel.x = 1;  
-  spelunky.scale.x = 1;
-  spelunky.vel.y = 0;
-}
-
-function walkUp()
-{
-  green.changeAni('walkUp');
-  green.vel.y = -1;
-  green.vel.x = 0;
-
-  ninja.changeAni('walkUp');
-  ninja.vel.y = -1;
-  ninja.vel.x = 0;
-
-  spelunky.changeAni('walkUp');
-  spelunky.vel.y = -1;
-  spelunky.vel.x = 0;
-}
+let spelunkyCharacter;
