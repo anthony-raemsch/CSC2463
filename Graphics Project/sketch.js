b252dcdf9d2557e351511b9e65f7554c704a5c0d
gamingar-tony <araems1@lsu.edu>
2024-02-19 11:32:46 -0600

    Added Round Bug Sprite

--- Graphics Project/sketch.js
+++ Graphics Project/sketch.js
@@ -1,7 +1,19 @@
-function setup() {
-  createCanvas(400, 400);
+function setup()
+{
+  createCanvas(2519, 1338);
 }
 
-function draw() {
-  background(220);
+function draw()
+{
+  background(255);
 }
+
+function preload()
+{
+  roundBug = new Sprite(200, 200, 80, 80);
+  roundBug.spriteSheet = 'assets/Round Bug.png';
+  roundBug.anis.frameDelay = 8;
+}
+
+let animations;
+let roundBug;
