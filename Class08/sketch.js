4cb3342e00de628ce465d85276f932626b5ae165
gamingar-tony <araems1@lsu.edu>
2024-02-20 14:07:25 -0600

    Finished Buttons

--- Class08/sketch.js
+++ Class08/sketch.js
@@ -1,7 +1,40 @@
-function setup() {
+function setup()
+{
   createCanvas(400, 400);
+
+  let button1 = createButton('Popcorn Maker');
+  button1.position(85, 150);
+  button1.mousePressed(() => soundFX.player('popcorn').start());
+
+  let button2 = createButton('Water Fall');
+  button2.position(205, 150);
+  button2.mousePressed(() => soundFX.player('water').start());
 }
 
-function draw() {
+function draw()
+{
   background(220);
+  // text('Press Q or W for sound!', 120, 150);
 }
+
+function preload()
+{
+  let soundFX = new Tone.Players
+  ({
+    popcorn: "assets/popcorn.mp3",
+    water: "assets/water.mp3"
+  }).toDestination(); // tells your audio to go to your speakers
+}
+
+// function keyPressed()
+// {
+//   if (keyCode === 'q')
+//   {
+//     soundFX.player("popcorn").start();
+//   }
+
+//   else if (key === 'w')
+//   {
+//     soundFX.player("water").start();
+//   }
+// }
