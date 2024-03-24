a49d5fe7fb1bbbd1601012b35485722e1fdc7ff1
gamingar-tony <araems1@lsu.edu>
2024-03-24 12:58:56 -0500

    Relatively finished Sound Project

--- Sound Project/sketch.js
+++ Sound Project/sketch.js
@@ -1,9 +1,26 @@
-let colorSpectrum;
-let selectedColor;
-let sizeOfSquare;
-let drawing;
-let dex;
-let i;
+let colorSpectrum, selectedColor, sizeOfSquare, drawing, dex, i, sine, melody, nature;
+
+let sequence = [['A4', 'A4', 'A4'], 'A4', 'F4', 'G4', ['A4', 'G4'], 'A4'];
+
+nature = new Tone.Players
+({
+    'nature' : 'assets/nature.mp3'
+});
+
+sine = new Tone.Synth({
+    oscillator:
+    {
+        type: "sine"
+    }
+}).toDestination;
+
+melody = new Tone.Sequence(function (time, note)
+{
+    sine.triggerAttackRelease(note, 0.8);
+}, sequence, "8n");
+
+Tone.Transport.start();
+Tone.Transport.bpm.value = 60;
 
 function setup()
 {
@@ -33,6 +50,8 @@ function mouseClicked()
         drawing = false;
         dex = Math.floor(mouseY / sizeOfSquare);
         selectedColor = colorSpectrum[dex];
+        Tone.start();
+        melody.start();
     }
 }
 
@@ -41,12 +60,14 @@ function mousePressed()
     if (mouseX > sizeOfSquare && mouseX < width && mouseY > 0 && mouseY < height)
     {
         drawing = true;
+        nature.player('nature').start();
     }
 }
 
 function mouseReleased()
 {
     drawing = false;
+    nature.player('nature').stop();
 }
 
 function colorPalleteDrew()
